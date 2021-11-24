6c05df754bebbc7bcdc620d0d96bba8486125059
cuxhausen <cuxhausen@mail.ru>
2021-11-24 22:37:49 +0600

    added list true domain name

--- Email_Slicer.py
+++ Email_Slicer.py
@@ -2,7 +2,10 @@ email = input("Enter your email address: ").strip()
 
 e_list = list(email)
 element = '@'
-
+true_domain = ['hotmail.com', 'live.com', 'msn.com', 'yahoo.com', 'ymail.com', 'rocketmail.com',
+            'yahoomail.com', 'yahoo-inc.com', 'gmail.com', 'googlemail.com', 'aol.com', 'mail.ru',
+            'list.ru', 'bk.ru', 'inbox.ru', 'corp.mail.ru', 'yandex.ru', 'rambler.ru', 'qip.ru',
+            'bigmir.net', 'ukr.net', 'kimo.com', ]
 if element not in e_list:
     print("You entered the wrong email address")
 else:
