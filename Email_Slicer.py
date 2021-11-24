1bf2aa1fd531b529bd47906b3086873add958387
cuxhausen <cuxhausen@mail.ru>
2021-11-24 22:19:28 +0600

    added code

--- Email_Slicer.py
+++ Email_Slicer.py
@@ -1,2 +1,15 @@
-def main():
-    pass
+email = input("Enter your email address: ").strip()
+
+e_list = list(email)
+element = '@'
+
+if element not in e_list:
+    print("You entered the wrong email address")
+else:
+    index = email.index('@')
+    username = email[:index]
+    domain = email[index+1:]
+
+    print("\nYour email address: ", email)
+    print("Username: ", username)
+    print("Domain name: ", domain)
