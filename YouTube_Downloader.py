be6a6e9d5d2f2adee6e3b00b94e44e3adb88a000
cuxhausen <cuxhausen@mail.ru>
2021-12-02 19:17:18 +0600

    added the code to the file

--- YouTube_Downloader.py
+++ YouTube_Downloader.py
@@ -1,4 +1,4 @@
-import pytube
+import pytube # pip install pytube, pytube3
 from pytube import YouTube
 
 link = input("Enter the link: ")
@@ -32,3 +32,13 @@ ys = yt.streams.get_by_itag('22') # We can also choose any stream by the help of
 ys.download() # Upload a video to the current directory
 
 # ys.download('location')
+
+"""
+import pytube
+video_link = imput("Enter your video link: ") # https://www.youtube.com/watch?v=8ZpVwAeLzm4
+yt = pytube.YouTube(video_link)
+videos = yt.videos
+video = yt.get('mp4', '720p') # we filter the video by format and quality
+path = '/Users/Oltjano/Desktop'
+video.download(path)
+"""
